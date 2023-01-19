1e578c5deb35b258f3ceaf77aa12d06f5d7026e0
BartoszLitwa <crytech.sim@gmail.com>
2023-01-19 22:31:19 +0100

    Solutions as of 01.19.2023 10:30pm

---
+++ Greedy/0846. Hand of Straights.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public bool IsNStraightHand(int[] hand, int groupSize) {
+        if(hand.Length % groupSize != 0)
+            return false;
+
+        var dict = new Dictionary<int, int>();
+        var sorted = new List<int>();
+        for(int i = 0; i < hand.Length; i++){ // Count the occurances
+            if(dict.ContainsKey(hand[i])){
+                dict[hand[i]]++;
+            } else {
+                dict.Add(hand[i], 1);
+                if(!sorted.Contains(hand[i])) // Add only 1 copy
+                    // since we will be removing it only after there are no copies left
+                    sorted.Add(hand[i]);
+            }
+        }
+        sorted.Sort();
+
+        while(sorted.Count > 0){
+            var first = sorted[0];
+            for(int i = first; i < first + groupSize; i++){
+                if(!dict.ContainsKey(i)) // We cannot create consecutive group
+                    return false;
+
+                if(--dict[i] == 0){ // Check if it was the last one
+                    if(i != sorted[0]) // its not the smallest available
+                        return false;
+
+                    sorted.RemoveAt(0); // Remove it from the beginning
+                }
+            }
+        }
+
+        return true;
+    }
+}
