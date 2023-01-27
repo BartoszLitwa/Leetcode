a915a08b09f070139a4912d4c79a6b6380719f90
BartoszLitwa <crytech.sim@gmail.com>
2023-01-28 00:07:12 +0100

    Solutions as of 01.28.2023 12:30am

--- Math & Geometry/0054. Spiral Matrix.cs
+++ Math & Geometry/0054. Spiral Matrix.cs
@@ -4,29 +4,32 @@ public class Solution {
         int top = 0, bottom = matrix.Length - 1;
 
         var res = new List<int>();
-        while(left < right && top < bottom){
-            Console.WriteLine("test");
+        while(left <= right && top <= bottom){
             // Get top row
             for(int i = left; i <= right; i++){
                 res.Add(matrix[top][i]);
             }
+            top++;
+
             // Get right column
-            for(int i = top + 1; i <= bottom; i++){
+            for(int i = top; i <= bottom; i++){
                 res.Add(matrix[i][right]);
             }
+            right--;
+            if(!(left <= right && top <= bottom))
+                break;
+
             // Get bottom row
-            for(int i = right - 1; i >= left; i--){
+            for(int i = right; i > left - 1; i--){
                 res.Add(matrix[bottom][i]);
             }
+            bottom--;
+
             // Get left column
-            for(int i = bottom - 1; i > top; i--){
+            for(int i = bottom; i > top - 1; i--){
                 res.Add(matrix[i][left]);
             }
-
             left++;
-            right--;
-            top++;
-            bottom--;
         }
         return res;
     }
