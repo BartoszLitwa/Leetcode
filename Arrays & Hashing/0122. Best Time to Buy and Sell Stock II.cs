e0d510ff5489bc364c83025ce24700641eef01f2
BartoszLitwa <crytech.sim@gmail.com>
2023-03-12 19:03:05 +0100

    Solutions as of 03.12.2023 7pm

---
+++ Arrays & Hashing/0122. Best Time to Buy and Sell Stock II.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        var profit = 0;
+
+        for(int i = 1; i < prices.Length; i++){
+            if(prices[i] > prices[i - 1]){ // Profit
+                profit += (prices[i] - prices[i - 1]);
+            }
+        }
+        return profit;
+    }
+}
