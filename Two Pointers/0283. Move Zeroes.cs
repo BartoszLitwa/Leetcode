4a085b5655dc44f93dcc76d4d66f4500d6b03577
BartoszLitwa <crytech.sim@gmail.com>
2023-03-15 16:43:51 +0100

    Solutions as of 03.15.2023 5pm

---
+++ Two Pointers/0283. Move Zeroes.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        int left = 0;
+        for(int right = 0; right < nums.Length; right++){
+            if(nums[right] != 0){ // if not zero, swap
+                int temp = nums[left];
+                nums[left++] = nums[right];
+                nums[right] = temp;
+            }
+        }
+    }
+}
