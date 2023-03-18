f912a41090ada18b6d4623ee765a90de909e7354
BartoszLitwa <crytech.sim@gmail.com>
2023-03-18 18:07:54 +0100

    Solutions as of 03.18.2023 6pm

---
+++ Binary Search/0374. Guess Number Higher or Lower.cs
@@ -0,0 +1,26 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution : GuessGame {
+    public int GuessNumber(int n) {
+        int left = 1, right = n;
+        while(left <= right) {
+            int mid = left + ((right - left) >> 1); // Divide by 2
+            var res = guess(mid);
+            if(res == 0)
+                return mid;
+            else if(res == 1)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return 0;
+    }
+}
