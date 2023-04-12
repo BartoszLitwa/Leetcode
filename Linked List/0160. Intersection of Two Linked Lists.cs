8b88dfa1aea267fa45367fc5f4ad89a4afe85784
BartoszLitwa <crytech.sim@gmail.com>
2023-04-12 21:45:06 +0200

    Solutions as of 04.12.2023 9:45pm

---
+++ Linked List/0160. Intersection of Two Linked Lists.cs
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA is null || headB is null) // Base case
+            return null;
+
+        ListNode a = headA, b = headB;
+
+        while(a != b){ // Until a and b are the same
+            // If we reach the end of a list, then we start at the beginning of the list
+            a = a is null ? headA : a.next; // Otherwise we move to the next node
+            b = b is null ? headB : b.next;
+        }
+
+        return a; // Return the intersection node
+    }
+}
